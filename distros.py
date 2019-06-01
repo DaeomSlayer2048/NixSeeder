e7c5cf249ca0b31e8559d0d1c6990b785ab346db
Jacob Hanafin <jhanafin@Workstation.hanafin.lan>
2019-06-01 12:41:35 -0500

    Add centos

--- distros.py
+++ distros.py
@@ -11,6 +11,7 @@ def parse_ftp(base_url, torrent_format, version_format):
     for version in versions:
         version_url = base_url + version + '/'
         version_urls.append(version_url)
+    #Remove dupes
     version_urls = list(set(version_urls))
     #Get torrents in version folder
     for version_url in version_urls:
@@ -39,12 +40,31 @@ def get_arch():
 def get_fedora():
     urls = []
     base_url = "https://torrent.fedoraproject.org/torrents/"
-    torrent_format = re.compile("(?:\"\>)(Fedora.+.torrent)")
+    torrent_format = re.compile("(?:\"\>)(Fedora.+\.torrent)")
     #Main page
     response = urllib.request.urlopen(base_url).read().decode('utf-8')
     torrents = torrent_format.findall(response)
     for torrent in torrents:
         print("Found: %s" % (torrent))
         url = base_url + torrent
+        urls.append(url)
+    return urls
+
+def get_centos():
+    urls = []
+    base_url = "ftp://mirror.rackspace.com/CentOS/"
+    torrent_format = re.compile("CentOS-.+\.torrent")
+    version_format = re.compile("[0-9]\.[0-9]\.[0-9]+|[0-9]\.[0-9]+")
+    #Main page
+    response = urllib.request.urlopen(base_url).read().decode('utf-8')
+    versions = version_format.findall(response)
+    #Only the latest is stored on this server
+    latest = max(versions)
+    version_url = base_url + latest + '/isos/x86_64/'
+    response = urllib.request.urlopen(version_url).read().decode('utf-8')
+    torrents = torrent_format.findall(response)
+    for torrent in torrents:
+        url =  version_url + torrent
+        print("Found: %s" % (torrent))
         urls.append(url)
     return urls
